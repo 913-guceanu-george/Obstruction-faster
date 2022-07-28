486804e75d92e97d6345d481d45d802943859ce3
Marian Guceanu <george.guceanu@stud.ubbcluj.ro>
2022-07-29 02:28:23 +0300

    - many

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marian/Code/Obstruction-faster/Obs
+# Build directory: /home/marian/Code/Obstruction-faster/Obs/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
